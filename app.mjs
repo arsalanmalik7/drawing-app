f833777d8bc205a904839a169365e0e4cdecbad7
arsalanmalik7 <arsalantech277@gmail.com>
2023-05-24 01:36:45 +0500

    almost done

--- app.mjs
+++ app.mjs
@@ -1,146 +1,177 @@
-window.addEventListener('load', () => {
+let canvas = document.querySelector("#screen");
+let ctx = canvas.getContext("2d");
 
-    let canvas = document.querySelector("#screen");
-    let ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+let isDrawing = false;
+let prevMouseX, prevMouseY, snapShot;
+let selectedTool = 'pointer';
 
 
-    // image upload
-    let upload = document.querySelector("#uploadImage");
-    upload.addEventListener('change', handleImageUpload);
-    function handleImageUpload(e) {
-        const reader = new FileReader();
-        reader.onload = function (event) {
-            const img = new Image();
-            // Event listener for image load
-            img.onload = function () {
-                canvas.width = img.width;
-                canvas.height = img.height;
-                // Draw the image on the canvas
-                ctx.drawImage(img, 0, 0);
-            };
-            // Set the source of the image
-            img.src = event.target.result;
-        };
-        // Read the selected file as Data URL
-        reader.readAsDataURL(e.target.files[0]);
+
+const drawRect = (event) => {
+    ctx.strokeRect(event.offsetX, event.offsetY, prevMouseX - event.offsetX, prevMouseY - event.offsetY);
+}
+
+const drawCircle = (event) => {
+    ctx.beginPath();
+    let radius = Math.sqrt(Math.pow((prevMouseX - event.offsetX), 2) + Math.pow((prevMouseY - event.offsetY), 2))
+    ctx.arc(prevMouseX, prevMouseY, radius, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function startDrawing(event) {
+    isDrawing = true;
+    prevMouseX = event.offsetX;
+    prevMouseY = event.offsetY;
+    ctx.beginPath();
+    let sizeInput = document.querySelector("#sizeOfPointer");
+    sizeInput.addEventListener('input', function () {
+        size = sizeInput.value;
+        ctx.lineWidth = size;
+    });
+    snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    let colorPickers = document.querySelector(".colors").value;
+    ctx.strokeStyle = colorPickers;
+    ctx.lineCap = 'round'
+    ctx.stroke();
+}
+
+
+
+function draw(event) {
+    if (!isDrawing) return;
+    ctx.putImageData(snapShot, 0, 0);
+    let colorPickers = document.querySelector(".colors").value;
+
+    if (selectedTool === 'pointer' || selectedTool === "eraser") {
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : colorPickers;
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+    } else if (selectedTool === 'square') {
+        drawRect(event);
+        ctx.stroke();
+    } else if (selectedTool === 'circle') {
+        drawCircle(event);
     }
-
-
-
-
-    let crop = document.querySelector("#crop");
-    let eraser = document.querySelector("#eraser");
-    let pencil = document.querySelector("#pencil");
-    let straightLine = document.querySelector("#line");
-    let square = document.querySelector("#square");
-    let circle = document.querySelector("#circle");
-    let addtext = document.querySelector("#addText");
-    let fillBucket = document.querySelector("#bucket");
-
-    // clear canvas
-    let clearBoard = document.querySelector("#clearBoard");
-    clearBoard.addEventListener('click', function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+toolBtn = document.querySelectorAll(".tool");
+toolBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        selectedTool = btn.id;
+        console.log(selectedTool);
     })
-
-    let isDrawing = false;
-
-    canvas.addEventListener('mousedown', startDrawing);
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mouseup', stopDrawing);
-    canvas.addEventListener('mouseout', stopDrawing);
-
-
-
-    canvas.addEventListener("mouseover", function () {
-        canvas.style.cursor = "crosshair";
-    });
-
-
-    function startDrawing(event) {
-        isDrawing = true;
-        let rect = canvas.getBoundingClientRect();
-        let x = event.clientX - rect.left;
-        let y = event.clientY - rect.top;
-
-        let colorPickers = document.querySelector(".colors").value;
-        ctx.strokeStyle = colorPickers;
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.lineTo(x, y);
-        let sizeInput = document.querySelector("#sizeOfPointer");
-        sizeInput.addEventListener('input', function () {
-            size = sizeInput.value;
-
-            ctx.lineWidth = size;
-        });
-
-        ctx.lineCap = 'round'
-        ctx.stroke();
-    }
-
-    function draw(event) {
-        if (isDrawing) {
-            let rect = canvas.getBoundingClientRect();
-            let x = event.clientX - rect.left;
-            let y = event.clientY - rect.top;
-
-            ctx.lineTo(x, y);
-            ctx.stroke();
-        }
-
-    }
-
-    function stopDrawing(event) {
-        isDrawing = false;
-        ctx.stroke();
-        draw(event);
-    }
-    let squareStartX = 0;
-    let squareStartY = 0;
-    let squareSize = 0;
-    let isDrawingLine = false;
-    let isDrawingSquare = false;
-    // Event listener for the square button
-    let squareButton = document.querySelector("#square");
-    squareButton.addEventListener("click", function () {
-        isDrawingSquare = !isDrawingSquare;
-        squareButton.classList.toggle("active");
-
-        if (isDrawingSquare) {
-            canvas.addEventListener("mousedown", startDrawingSquare);
-            canvas.addEventListener("mousemove", drawSquare);
-            canvas.addEventListener("mouseup", endDrawingSquare);
-            canvas.addEventListener("mouseout", stopDrawing);
-        } else {
-            canvas.removeEventListener("mousedown", startDrawingSquare);
-            canvas.removeEventListener("mousemove", drawSquare);
-            canvas.removeEventListener("mouseup", endDrawingSquare);
-            canvas.removeEventListener("mouseout", stopDrawing);
-        }
-    });
-
-
-    function startDrawingSquare(event) {
-        isDrawing = true;
-        squareStartX = event.clientX;
-        squareStartY = event.clientY;
-        lastX = squareStartX;
-        lastY = squareStartY;
-        saveCanvasState();
-    }
-
-    document.querySelector('#save').addEventListener('click', function () {
-        canvas.toBlob(function (blob) {
-            saveAs(blob, 'output.png');
-        }, 'image/png');
-    });
-
-
 });
+let square = document.querySelector("#square");
+square.addEventListener('click', () => {
+    isDrawing = true;
+})
+
+function stopDrawing(event) {
+    isDrawing = false;
+}
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseout', stopDrawing);
+
+let textBox = null;
+
+// Add event listener to the "Add Text Box" button
+const addTextBoxButton = document.querySelector('#addText');
+addTextBoxButton.addEventListener('click', () => {
+    createTextBox();
+});
+
+// Function to create the text box element
+function createTextBox() {
+    // Create the text box input element
+    textBox = document.createElement('input');
+    textBox.type = 'text';
+    textBox.placeholder = 'Enter text';
+    textBox.style.position = 'absolute';
+    textBox.style.top = '10px';
+    textBox.style.left = '10px';
+
+    // Append the text box to the body or a container element
+    document.body.appendChild(textBox);
+
+    // Focus on the text box to allow immediate input
+    textBox.focus();
+}
+
+// Function to handle user input on the text box
+function handleTextInput() {
+    if (textBox) {
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Set the font properties
+        ctx.font = '24px Arial';
+        ctx.fillStyle = 'black';
+
+        // Draw the text on the canvas
+        ctx.fillText(textBox.value, 50, 50);
+    }
+}
+
+// Event listener for handling text input changes
+document.addEventListener('input', handleTextInput);
+
+// image upload
+let upload = document.querySelector("#uploadImage");
+upload.addEventListener('change', handleImageUpload);
+function handleImageUpload(e) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const img = new Image();
+        // Event listener for image load
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            // Draw the image on the canvas
+            ctx.drawImage(img, 0, 0);
+        };
+        // Set the source of the image
+        img.src = event.target.result;
+    };
+    // Read the selected file as Data URL
+    reader.readAsDataURL(e.target.files[0]);
+}
+
+
+
+
+
+// clear canvas
+let clearBoard = document.querySelector("#clearBoard");
+clearBoard.addEventListener('click', function dontRepeat() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 0;
+})
+canvas.addEventListener("mouseover", function () {
+    canvas.style.cursor = "crosshair";
+});
+
+
+document.querySelector('#save').addEventListener('click', function () {
+    canvas.toBlob(function (blob) {
+        saveAs(blob, 'output.png');
+    }, 'image/png');
+});
+
+
+
+
+
+
+
+
+
 
 
 
